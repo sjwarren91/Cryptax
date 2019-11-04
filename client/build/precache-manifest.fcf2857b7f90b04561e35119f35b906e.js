942575d664fe0350f57bd1ebe16ddc38a4d38844
Sam Warren <sjwarren91@gmail.com>
2019-11-05 10:28:09 +1100

    folder structure and testing Binance API

---
+++ client/build/precache-manifest.fcf2857b7f90b04561e35119f35b906e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ec693eb091fce0629614",
+    "url": "/static/js/main.6591fc41.chunk.js"
+  },
+  {
+    "revision": "66bbc66fb81735725762",
+    "url": "/static/js/2.4798774d.chunk.js"
+  },
+  {
+    "revision": "ec693eb091fce0629614",
+    "url": "/static/css/main.ba2afae9.chunk.css"
+  },
+  {
+    "revision": "d42f906f0243d9c6b9053c21e11efb31",
+    "url": "/index.html"
+  }
+];
